41647dd180c8a00e69ea33957d4f6c11790a7eb4
Guilherme Pallas <guileui_pallas@hotmail.com>
2024-07-08 18:50:42 -0300

    Created relation between model classes

--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -12,5 +12,7 @@ namespace ApiCatalogo.Models
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
 
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
     }
 }

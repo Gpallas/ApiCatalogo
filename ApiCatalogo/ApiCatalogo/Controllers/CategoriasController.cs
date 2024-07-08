91d88ee9580b22052f5c0cfedf96b8428eea1734
Guilherme Pallas <guileui_pallas@hotmail.com>
2024-07-08 19:32:15 -0300

    Controllers created

---
+++ ApiCatalogo/ApiCatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,17 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}

8a68d905960b8b502af495421b171674523fb888
Guilherme Pallas <guileui_pallas@hotmail.com>
2024-07-11 19:40:39 -0300

    Created UnitOfWork

---
+++ ApiCatalogo/ApiCatalogo/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCatalogo.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IProdutoRepository ProdutoRepository { get; }
+        ICategoriaRepository CategoriaRepository { get; }
+        void Commit();
+    }
+}
